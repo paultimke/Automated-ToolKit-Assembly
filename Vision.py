4df15b1ac4d69e336aa56c50ef6fb74480a4433f
Paul Timke <87957114+paultimke@users.noreply.github.com>
2022-05-27 18:00:55 -0600

    Eliminated Vimba functions
    Deleted vimba related functions as they're not needed in this branch. Also changed the name of get_ref_path to calibrate_cam() to make it more understandable

--- Vision.py
+++ Vision.py
@@ -1,9 +1,7 @@
 from array import array
 from typing import Tuple
-#from pymba import Vimba, VimbaException
-#from pymba import Frame
-from cv2 import Mat
 import cv2
+from cv2 import Mat
 from datetime import datetime
 import os
 import numpy as np
@@ -12,12 +10,11 @@ from imutils import perspective
 from imutils import contours
 from scipy.spatial import distance as dist
 
-# todo add more colours
-PIXEL_FORMATS_CONVERSIONS = {
-    'BayerRG8': cv2.COLOR_BAYER_RG2RGB,
-}
+# CONSTANTS
+DEBUG = False
 
-def get_image_path(path:str) -> None:
+
+def get_image_from_path(path:str) -> None:
     """ Get Image from specified Path Function.
     Return image Mat and image size (pixels)
     
@@ -28,45 +25,6 @@ def get_image_path(path:str) -> None:
 
     return image, size
 
-def get_image_cam(source:int) -> None:
-    """ Get image from camera source Function.
-    Return image Mat and image size (pixels)
-    
-    @source parameter int, 0-default(webcam), 1..n-next source
-    """
-    capture = cv2.VideoCapture(source)
-    ret, frame = capture.read()
-
-    return frame
-
-def get_image_Vimba() -> Frame:
-    """ Get image from Vimba Function.
-    
-    Use Vimba functions to take picture from Allied Vision cameras 
-    """
-    with Vimba() as vimba:
-        #init camera
-        camera = vimba.camera(0)
-        camera.open()
-        camera.arm('SingleFrame')
-
-        # capture a single frame, more than once if desired
-        try:
-            #take picture
-            frame = camera.acquire_frame()
-            #foto = get_frame(frame)
-        except VimbaException as e:
-            # rearm camera upon frame timeout
-            if e.error_code == VimbaException.ERR_TIMEOUT:
-                print(e)
-                camera.disarm()
-                camera.arm('SingleFrame')
-            else:
-                raise
-        camera.disarm()
-        camera.close()
-
-    return frame
 
 def crop_image(img:Mat, Xi:int, Xf:int, Yi:int, Yf:int) -> Mat:
     """ Crop image Function.
@@ -81,7 +39,7 @@ def crop_image(img:Mat, Xi:int, Xf:int, Yi:int, Yf:int) -> Mat:
 
     return img
 
-def save_image(image:Mat, name:str, folder:str, show:bool) ->None:
+def save_image(image:Mat, name:str, folder:str) ->None:
     """ Save Image Function.
     Get the date and time from computer, then save the 
     given image with an specific name and path folder
@@ -95,17 +53,17 @@ def save_image(image:Mat, name:str, folder:str, show:bool) ->None:
     #Get Date
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y__%Hh-%Mm-%Ss")
-    if show:
+    if DEBUG:
         print("date and time =", dt_string)
 
     #Save image to folder
     status=cv2.imwrite(f'{folder}/{name}_{dt_string}.png',image)
-    if status and show:
+    if status and DEBUG:
         print("Image written to file-system : ",status)
     elif not status:
         os.mkdir(folder)
         status=cv2.imwrite(f'{folder}/{name}_{dt_string}.png',image)
-        if show:
+        if DEBUG:
             print("Image written to file-system : ",status)
     
 
@@ -135,33 +93,8 @@ def RGB2binary(img: Mat) -> Mat:
 
     return img
     
-def vimba2binary(img: Frame) ->Mat:
-    """ Gray to Binary Function. Allied Vision Camera
-    Process a grayscale Vimba image with morphology techniques to convert
-    it into a binary array 
-    @img parameter image (3 dimensions Mat)"""
 
-    #pymba image to numpy array
-    img = img.buffer_data_numpy()
-    #blur image
-    og_img = cv2.medianBlur(img,1)
-    img = cv2.medianBlur(img,7)
-    #treshhold over 1st value will covert to 2nd value
-    _ ,img = cv2.threshold(img,45,255,cv2.THRESH_BINARY)
-    #arreglo chiquito para hacer operaciones morfologicas
-    kernel = np.ones((3,3),np.uint8)
-    #filtro de morfología abierto
-    img = cv2.morphologyEx(img,cv2.MORPH_OPEN,kernel) 
-    #transformación NO SE
-    img = cv2.distanceTransform(img,cv2.DIST_L2,5)
-    #reducir tamaño de objetos por factor
-    _, img =  cv2.threshold(img, 0.05*img.max(),255,0)
-    #cambiar formato de u32 a u8
-    img = np.uint8(img)
-
-    return img, og_img
-
-def count_objects_AnP(image:Mat, show:bool, show_more:bool) -> Mat:
+def count_objects_AnP(image:Mat) -> Mat:
     """ Count objects: Area and Perimeter Function.
     Count how many objects are detected in the given image, then shows 
     the info for each object: Area and Perimeter 
@@ -186,20 +119,22 @@ def count_objects_AnP(image:Mat, show:bool, show_more:bool) -> Mat:
             perimeter = cv2.arcLength(cnt, True)
             #print(len(approx))
             finalContours.append([area, perimeter])
-            if show_more:
+            if DEBUG:
                 print("Area",i,"= ", area)
                 print("Perim",i,"= ", round(perimeter,2),"\n")
         
     cnts = imutils.grab_contours(cnts)
-    if show:
+    if DEBUG:
         print("Objects in the image : ", i)
 
     return finalContours, cnts
 
+
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-def measure_first(cnts:array, reference:int) -> Mat:
+
+def measure_reference(cnts:array, reference:int) -> Mat:
     # sort the contours from left-to-right and initialize the
     # 'pixels per metric' calibration variable
     (cnts, _) = contours.sort_contours(cnts)
@@ -235,6 +170,7 @@ def measure_first(cnts:array, reference:int) -> Mat:
         pixelsPerMetric = (dB / reference)
 
     return pixelsPerMetric
+
 
 def measure_objects(image:Mat, cnts:array, px_cm:int) -> Mat:
     # sort the contours from left-to-right and initialize the
@@ -299,75 +235,28 @@ def measure_objects(image:Mat, cnts:array, px_cm:int) -> Mat:
             (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
             0.3, (255, 255, 255), 1)
 
-        DimA = round(dimA,2)
-        DimB = round(dimB,2)
+        DimA = round(dimA,4)
+        DimB = round(dimB,4)
 
+        # Populate the sizes list with detected dimensions
         sizes.append([DimA,DimB])
 
     return orig, sizes
 
 def img_detectSizes(ref_img : int) -> Tuple[Mat, list]:
-    #image = vs.get_image_cam(1)
-    #image = cv2.imread('tst_img3.png')
-    img = get_image_Vimba()
-    #img = RGB2binary(image)
-    img, og_img = vimba2binary(img)
-    _, contours = count_objects_AnP(img,1,0)
-    img, sizes = measure_objects(img, contours, ref_img)
-
-    #nums = print_count(og_img, contours)
-    #cv2.imshow('nums', nums)
-    #cv2.imshow('LEC',img)
-    #cv2.waitKey(0)
-    #save_image(img, 'test','Images\test_imgs',False)
-    #cv2.destroyAllWindows()
-    # Cut the head off the list because it's the reference object
-    return (og_img, sizes[1:])
-
-def print_count(img:Mat, objects:int) ->Mat:
-    # sort the contours from left-to-right and initialize the
-    # 'pixels per metric' calibration variable
-    (objects, _) = contours.sort_contours(objects)
-    #poner contornos y numeración
-    for (i, c) in enumerate(objects):
-        ((x, y), _) = cv2.minEnclosingCircle(c)
-        cv2.putText(img, "{}".format(i + 1), (int(x) - 41, int(y)+20),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        #cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
-    
-    return img 
-
-def print_typNcnt(img:Mat, objects:int, types:list) -> Mat:
-    # sort the contours from left-to-right and initialize the
-    # 'pixels per metric' calibration variable
-    (objects, _) = contours.sort_contours(objects)
-    #poner contornos y numeración
-    for (i, c) in enumerate(objects):
-        ((x, y), _) = cv2.minEnclosingCircle(c)
-        cv2.putText(img, "{}".format(i + 1), (int(x) - 41, int(y)+20),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        cv2.putText(img, types[i][1], (int(x) - 20, int(y)+40),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 1)
-    
-    return img
-
-def get_ref_path(path:str,reference:int) -> int:
-    img, _ = get_image_path(path)
+    img,_ = get_image_from_path("Images/Test_Imgs/tst_img1.png")
     img = RGB2binary(img)
-    _, cntz= count_objects_AnP(img,0,0)
-    px_m = measure_first(cntz,reference)
+    _, contours = count_objects_AnP(img)
+    img, sizes = measure_objects(img, contours, ref_img)
+    return (img, sizes)
+
+
+def calibrate_cam(path_to_img: str, reference: int) -> int:
+    img,_ = get_image_from_path(path_to_img)
+    img = RGB2binary(img)
+    _, cnts = count_objects_AnP(img)
+    px_m = measure_reference(cnts,reference)
 
     return px_m
 
-def res_vimba()-> None:
-    """ Restart Vimba Viewer settings to continous 
-    frame adquisition """
-    with Vimba() as vimba:
-        #init camera
-        camera = vimba.camera(0)
-        camera.open()
-        camera.arm('Continuous')
-        print('Ready')
-        camera.disarm()
-        camera.close()
 
